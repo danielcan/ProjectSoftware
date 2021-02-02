f00d022432dd6eb8d1bf2a8d9b99b9dfc7f36435
danielcac <danielcanc@gmail.com>
2021-02-01 22:24:07 -0600

    Login

--- ProjectSoftware/ProjectSoftware/General/Login.Designer.vb
+++ ProjectSoftware/ProjectSoftware/General/Login.Designer.vb
@@ -22,74 +22,15 @@ Partial Class Login
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         Me.Password = New System.Windows.Forms.Label()
         Me.Usuario = New System.Windows.Forms.Label()
         Me.txtPassw = New System.Windows.Forms.TextBox()
         Me.txtUsuario = New System.Windows.Forms.TextBox()
         Me.btnSalir = New System.Windows.Forms.Button()
         Me.btnEntrada = New System.Windows.Forms.Button()
-        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'Password
-        '
-        Me.Password.AutoSize = True
-        Me.Password.BackColor = System.Drawing.Color.Azure
-        Me.Password.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Password.Location = New System.Drawing.Point(49, 259)
-        Me.Password.Name = "Password"
-        Me.Password.Size = New System.Drawing.Size(78, 20)
-        Me.Password.TabIndex = 16
-        Me.Password.Text = "Password"
-        '
-        'Usuario
-        '
-        Me.Usuario.AutoSize = True
-        Me.Usuario.BackColor = System.Drawing.Color.AliceBlue
-        Me.Usuario.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Usuario.ForeColor = System.Drawing.Color.Black
-        Me.Usuario.Location = New System.Drawing.Point(49, 197)
-        Me.Usuario.Name = "Usuario"
-        Me.Usuario.Size = New System.Drawing.Size(64, 20)
-        Me.Usuario.TabIndex = 15
-        Me.Usuario.Text = "Usuario"
-        '
-        'txtPassw
-        '
-        Me.txtPassw.Location = New System.Drawing.Point(161, 261)
-        Me.txtPassw.Name = "txtPassw"
-        Me.txtPassw.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.txtPassw.Size = New System.Drawing.Size(153, 20)
-        Me.txtPassw.TabIndex = 14
-        '
-        'txtUsuario
-        '
-        Me.txtUsuario.Location = New System.Drawing.Point(161, 197)
-        Me.txtUsuario.Name = "txtUsuario"
-        Me.txtUsuario.Size = New System.Drawing.Size(153, 20)
-        Me.txtUsuario.TabIndex = 13
-        '
-        'btnSalir
-        '
-        Me.btnSalir.BackColor = System.Drawing.Color.OrangeRed
-        Me.btnSalir.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnSalir.Location = New System.Drawing.Point(199, 328)
-        Me.btnSalir.Name = "btnSalir"
-        Me.btnSalir.Size = New System.Drawing.Size(87, 62)
-        Me.btnSalir.TabIndex = 12
-        Me.btnSalir.Text = "Salir"
-        Me.btnSalir.UseVisualStyleBackColor = False
-        '
-        'btnEntrada
-        '
-        Me.btnEntrada.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnEntrada.Location = New System.Drawing.Point(78, 328)
-        Me.btnEntrada.Name = "btnEntrada"
-        Me.btnEntrada.Size = New System.Drawing.Size(85, 62)
-        Me.btnEntrada.TabIndex = 11
-        Me.btnEntrada.Text = "Entrar"
-        Me.btnEntrada.UseVisualStyleBackColor = True
         '
         'PictureBox2
         '
@@ -101,6 +42,65 @@ Partial Class Login
         Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox2.TabIndex = 10
         Me.PictureBox2.TabStop = False
+        '
+        'Password
+        '
+        Me.Password.AutoSize = True
+        Me.Password.BackColor = System.Drawing.Color.Azure
+        Me.Password.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Password.Location = New System.Drawing.Point(64, 277)
+        Me.Password.Name = "Password"
+        Me.Password.Size = New System.Drawing.Size(78, 20)
+        Me.Password.TabIndex = 22
+        Me.Password.Text = "Password"
+        '
+        'Usuario
+        '
+        Me.Usuario.AutoSize = True
+        Me.Usuario.BackColor = System.Drawing.Color.AliceBlue
+        Me.Usuario.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Usuario.ForeColor = System.Drawing.Color.Black
+        Me.Usuario.Location = New System.Drawing.Point(64, 215)
+        Me.Usuario.Name = "Usuario"
+        Me.Usuario.Size = New System.Drawing.Size(64, 20)
+        Me.Usuario.TabIndex = 21
+        Me.Usuario.Text = "Usuario"
+        '
+        'txtPassw
+        '
+        Me.txtPassw.Location = New System.Drawing.Point(176, 279)
+        Me.txtPassw.Name = "txtPassw"
+        Me.txtPassw.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassw.Size = New System.Drawing.Size(153, 20)
+        Me.txtPassw.TabIndex = 20
+        '
+        'txtUsuario
+        '
+        Me.txtUsuario.Location = New System.Drawing.Point(176, 215)
+        Me.txtUsuario.Name = "txtUsuario"
+        Me.txtUsuario.Size = New System.Drawing.Size(153, 20)
+        Me.txtUsuario.TabIndex = 19
+        '
+        'btnSalir
+        '
+        Me.btnSalir.BackColor = System.Drawing.Color.OrangeRed
+        Me.btnSalir.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSalir.Location = New System.Drawing.Point(214, 346)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(87, 62)
+        Me.btnSalir.TabIndex = 18
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = False
+        '
+        'btnEntrada
+        '
+        Me.btnEntrada.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEntrada.Location = New System.Drawing.Point(93, 346)
+        Me.btnEntrada.Name = "btnEntrada"
+        Me.btnEntrada.Size = New System.Drawing.Size(85, 62)
+        Me.btnEntrada.TabIndex = 17
+        Me.btnEntrada.Text = "Entrar"
+        Me.btnEntrada.UseVisualStyleBackColor = True
         '
         'Login
         '
@@ -121,12 +121,11 @@ Partial Class Login
         Me.PerformLayout()
 
     End Sub
-
+    Friend WithEvents PictureBox2 As PictureBox
     Friend WithEvents Password As Label
     Friend WithEvents Usuario As Label
     Friend WithEvents txtPassw As TextBox
     Friend WithEvents txtUsuario As TextBox
     Friend WithEvents btnSalir As Button
     Friend WithEvents btnEntrada As Button
-    Friend WithEvents PictureBox2 As PictureBox
 End Class
